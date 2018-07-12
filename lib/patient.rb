f7b1a2afb784a1f56554f1c65369ebfed6b2d423
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-12 17:58:55 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -4,3 +4,9 @@ class Patient
   def initialize(name)
     @name = name
   end
+
+  def new_appointment(doctor, date)
+    new_appointment = Appointment.new(doctor, date)
+    new_appointment.patient = self
+    new_appointment
+  end
