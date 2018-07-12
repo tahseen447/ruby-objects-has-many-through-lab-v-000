e797e92dbd3ddd49c88aa21109da05f7a23b23b9
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-12 19:18:18 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -7,10 +7,12 @@ class Patient
 
   def new_appointment(doctor, date)
     new_appointment = Appointment.new(self, doctor, date)
-    new_appointment
   end
 
   def doctors
     Appointment.all.map {|appointment| appointment.doctor}
   end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient = self}
 end
